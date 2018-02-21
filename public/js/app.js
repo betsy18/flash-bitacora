e91751b2e11114201aa866d2a880c7eb6aea5cef
betsy18 <betsy_dessire@hotmail.com>
2018-02-21 06:29:33 -0500

    Funcionalidad 3

--- public/js/app.js
+++ public/js/app.js
@@ -189,27 +189,25 @@ $(document).ready(function() {
     saveVideo();
   }
 
+
+
   function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: {
-        latitud: -34.397,
-        longitud: 150.644
-      },
-      zoom: 15
-    });
-    var infoWindow = new google.maps.InfoWindow({ map: map });
+    
   
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
+      var map = new google.maps.Map(document.getElementById('map'), {
+
+       navigator.geolocation.getCurrentPosition(function(position) {
         var pos = {
           lat: position.coords.latitude,
           lng: position.coords.longitude
         };
   
         infoWindow.setPosition(pos);
-        infoWindow.setContent('Location found.');
+        infoWindow.setContent('Localización');
         map.setCenter(pos);
+        var infoWindow = new google.maps.InfoWindow({map: map});
       }, function() {
         handleLocationError(true, infoWindow, map.getCenter());
       });
@@ -217,40 +215,14 @@ $(document).ready(function() {
       // Browser doesn't support Geolocation
       handleLocationError(false, infoWindow, map.getCenter());
     }
-  
-    var txtMap = document.getElementById('txtMap');
-    var btnAdd = document.getElementById('btnAdd');
-    new google.maps.places.Autocomplete(txtmap);
-    var directionsService = new google.maps.DirectionsService;
-    var directionsDisplay = new google.maps.DirectionsRenderer;
-  
-    directionsDisplay.setMap(map);
-  
-    var calculateAndDisplayRoute = function(directionsService, directionsDisplay) {
-      directionsService.route({
-        origin: txtmap.value,
-        travelMode: 'DRIVING',
-        unitSystem: google.maps.UnitSystem.METRIC
-      }, function(response, status) {
-        if (status === 'OK') {
-          directionsDisplay.setDirections(response);
-        } else {
-          window.alert('No encontramos una ruta');
-        }
-      });
-    };
-  
-    var trazarRuta = function() {
-      calculateAndDisplayRoute(directionsService, directionsDisplay);
-    };
-    btnRuta.addEventListener('click', trazarRuta);
-  
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-      infoWindow.setPosition(pos);
-      infoWindow.setContent(browserHasGeolocation ?
-        'Error: The Geolocation service failed.' :
-        'Error: Your browser doesn\'t support geolocation.');
-    }
+  }
+});
+     
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+      'Error: The Geolocation service failed.' :
+      'Error: Your browser doesn\'t support geolocation.');
   }
 
   function saveVideo() {
